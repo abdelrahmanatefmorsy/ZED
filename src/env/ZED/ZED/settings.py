864b8a1a34c8a7b7f7d1445096c3121d54413570
adel762 <140119141+adel762@users.noreply.github.com>
2024-12-30 22:45:25 +0200

    add style to page index

--- src/env/ZED/ZED/settings.py
+++ src/env/ZED/ZED/settings.py
@@ -117,7 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# M25
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ZED/static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
