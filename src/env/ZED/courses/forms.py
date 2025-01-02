2c370057f7e0448b6b6b37ebbfb6b9c6eddf490c
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2025-01-02 22:51:03 +0200

    Fix typo: corrected "wedgets" to "widgets" in form file

--- src/env/ZED/courses/forms.py
+++ src/env/ZED/courses/forms.py
@@ -23,7 +23,7 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'email']
-        wedgets = {
+        widgets = {
             'password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', 'type': 'Password'}),
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
             'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
